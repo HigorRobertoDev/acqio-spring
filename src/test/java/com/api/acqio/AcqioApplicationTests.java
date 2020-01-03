ba5be48126b14b756e69ebdd90d4d0799fb034c8
Higor Roberto <h.saposilva@gmail.com>
2020-01-02 21:44:43 -0300

    Commit inicial

---
+++ src/test/java/com/api/acqio/AcqioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.acqio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcqioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
